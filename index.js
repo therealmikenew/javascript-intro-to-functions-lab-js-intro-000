3e238905bc036ee4e064f973c28ded911cdb77e5
Michael Newman <therealmichaelnewman@gmail.com>
2021-05-19 15:05:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function shout (string) {
   return string.toUpperCase ()
 }
+
+/* describe('whisper(string)', function() {
+  it('receives one argument and returns it in all lowercase', function() {
+    expect(whisper('HELLO')).toEqual('hello')
+  })
+})*/
+
+function whisper (string) {
+  return.string.toLowerCase ()
+}
