60eca3d0333db8a5199f1bd88e3021ddb8876e8d
Michael Newman <therealmichaelnewman@gmail.com>
2021-05-19 15:29:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,3 +28,19 @@ function whisper (string) {
 function logShout(string) {
   console.log(string.toUpperCase())
 }
+
+/* describe('logWhisper(string)', function() {
+  it('takes a string argument and logs it in all lowercase using console.log()', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logWhisper('HELLO')
+
+    expect(spy).toHaveBeenCalledWith('hello')
+
+    console.log.restore()
+  })
+}) */
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
